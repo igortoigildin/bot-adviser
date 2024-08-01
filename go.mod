77abbbe53f1b1e4a44d956a2136d02f2b442a5bb
Igor Toigildin <toygildinigor@gmail.com>
2024-08-01 13:21:18 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/igortoigildin/bot-adviser
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	_ = mustToken()
+}
+
+func mustToken() string {
+	token := flag.String("token-bot-token", "", "token for access to telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+	return *token
+}
